98554a0f89876d73676ee2dd2cdd1462541ccb71
小锦鲤 <553555614@qq.com>
2025-07-15 23:20:28 +0800

    sing-box 定制模板

---
+++ sing-box/1.11.x/sing-box-mine.js
@@ -0,0 +1,53 @@
+const { type, name } = $arguments
+const compatible_outbound = {
+  tag: 'COMPATIBLE',
+  type: '➡️ 直连',
+}
+
+let compatible
+let config = JSON.parse($files[0])
+let proxies = await produceArtifact({
+  name,
+  type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
+  platform: 'sing-box',
+  produceType: 'internal',
+})
+
+config.outbounds.push(...proxies)
+
+config.outbounds.map(i => {
+  if (['🚀 节点选择', '♻️ 所有-自动'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies))
+  }
+  if (['🇭🇰 香港-自动', '🪂 港台日新-自动'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|🇭🇰/i))
+  }
+  if (['🇨🇳 台湾-自动', '🪂 港台日新-自动'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼/i))
+  }
+  if (['🇯🇵 日本-自动', '🪂 港台日新-自动'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
+  }
+  if (['🇸🇬 新加坡-自动', '🪂 港台日新-自动'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i))
+  }
+  if (['🇺🇸 美国-自动'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
+  }
+})
+
+config.outbounds.forEach(outbound => {
+  if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
+    if (!compatible) {
+      config.outbounds.push(compatible_outbound)
+      compatible = true
+    }
+    outbound.outbounds.push(compatible_outbound.tag);
+  }
+});
+
+$content = JSON.stringify(config, null, 2)
+
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
